7efdbd4b32a0178934d6ccc11d9de66040e44212
Christian Nicolai <christian.nicolai@camunda.com>
2019-07-17 14:02:56 +0200

    project: initial draft

---
+++ README.md
@@ -0,0 +1,27 @@
+# vault-dump-kv2
+
+Dump your [Hashicorp Vault](https://www.vaultproject.io/) KV version 2 secrets engine contents to a file.
+Inspired by [vault-backup](https://github.com/shaneramey/vault-backup) but updated to work with KV version 2 only. Not guaranteed to be consistent.
+
+# Environment Variables
+
+In addition to the environment variables like `VAULT_ADDR` that the [hvac Python client for Hashicorp Vault](https://python-hvac.org/) implicitly uses, the following are used by this script:
+
+- `PYTHONIOENCODING` is used to ensure your keys are exported in valid encoding, make sure to use the same during import/export
+- `VAULT_DUMP_MOUNTPOINT` optionally passed as [`mount_point` argument](https://hvac.readthedocs.io/en/stable/usage/secrets_engines/kv_v2.html) to the hvac Python client
+
+# Setup
+
+You need to have the `vault` CLI tool and `pip` installed.
+
+```sh
+vault login # with auth method of your choice
+
+pip install -r requirements.txt
+
+export PYTHONIOENCODING="utf-8"
+export VAULT_DUMP_MOUNTPOINT="/mysecrets/"
+python vault-dump-kv2.py > mysecrets.txt
+```
+
+The generated script can be run with `sh mysecrets.txt` but beware that all keys in the target Vault will be overwritten on import!
---
+++ vault-dump-kv2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import datetime
+import os
+import sys
+
+import hvac
+
+# Source: https://hvac.readthedocs.io/en/stable/usage/secrets_engines/kv_v2.html
+client = hvac.Client()
+
+def print_secret(path, mountpoint):
+  content = client.secrets.kv.v2.read_secret_version(path, mount_point=mountpoint)['data']['data']
+
+  print ("vault kv put {}{}".format(vault_dump_mountpoint, path), end='')
+  for key in sorted(content.keys()):
+    value = content[key]
+    try:
+      value = value.encode("utf-8")
+    except AttributeError:
+      value = value
+    print (" {0}={1}".format(key, repr(value)), end='')
+  print ()
+
+def recurse_secrets(path_prefix, mountpoint):
+  keys = client.secrets.kv.v2.list_secrets(path_prefix, mount_point=mountpoint)['data']['keys']
+  for key in keys:
+    item_path = path_prefix + key
+    if key.endswith('/'):
+      recurse_secrets(item_path, mountpoint)
+    else:
+      print_secret(item_path, mountpoint)
+
+
+vault_dump_mountpoint = os.environ.get('VAULT_DUMP_MOUNTPOINT', '/secret/')
+
+print ('#')
+print ('# vault-dump-kv2.py backup')
+print ("# backup date: {} UTC".format(datetime.datetime.utcnow()))
+print ("# VAULT_DUMP_MOUNTPOINT setting: {}".format(vault_dump_mountpoint))
+print ('# STDIN encoding: {}'.format(sys.stdin.encoding))
+print ('# STDOUT encoding: {}'.format(sys.stdout.encoding))
+print ('#')
+print ('# WARNING: not guaranteed to be consistent!')
+print ('#')
+
+recurse_secrets('', vault_dump_mountpoint)
